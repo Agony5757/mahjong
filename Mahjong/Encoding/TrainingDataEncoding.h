8b79029dea0d2dcbb3cb7cd585a2c33a38c69b16
agony5757 <chenzhaoyun@iai.ustc.edu.cn>
2022-03-05 21:08:29 +0800

    update

--- Mahjong/Encoding/TrainingDataEncoding.h
+++ Mahjong/Encoding/TrainingDataEncoding.h
@@ -8,33 +8,26 @@ namespace TrainingDataEncoding {
 
 	using dtype = int8_t;
 	constexpr size_t size_player = 4,
-		col_hand = 0,
+		row_hand = 0,
 		size_hand = 6,
-		col_fulu = col_hand + size_hand,
+		row_fulu = row_hand + size_hand,
 		size_fulu = 6,
-		col_river = col_fulu + size_fulu * size_player,
+		row_river = row_fulu + size_fulu * size_player,
 		size_river = 10,
-		col_field = col_river + size_river * size_player,
+		row_field = row_river + size_river * size_player,
 		size_field = 10,
-		col_last = col_field + size_field,
-		size_last = 1;
+		row_last = row_field + size_field,
+		size_last = 1,
+		row_action = row_last + size_last,
+		size_action = 12,
+		row_oracle = row_action + size_action
+		;
 
 	constexpr size_t n_tile_types = 9 + 9 + 9 + 7;
-	constexpr size_t n_col = size_hand + size_fulu * size_player + size_river * size_player + size_field + size_last;
-	constexpr size_t n_row = n_tile_types;
-
-	constexpr int red_dora_flag = 1 << 6;
-	constexpr int field_wind_flag = 1 << 6;
-	constexpr int self_wind_flag = 1 << 7;
-	constexpr int riichi_flag = 1 << 7;
-	constexpr int naki_flag = 1 << 8;
-	constexpr int number_mask = 0b000111;
-	constexpr int fromhand_mask = 0b111000;
-	constexpr int dora_indicator_mask = 0b000111;
-	constexpr int dora_mask = 0b111000;
+	constexpr size_t n_row = size_hand + size_fulu * size_player + size_river * size_player + size_field + size_last;
+	constexpr size_t n_col = n_tile_types;
 
 	void encode_table(const Table& table, int pid, dtype* data);
-	void encode_action();
 };
 
 #endif
