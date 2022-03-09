b7c3a3a4f5640f482e7ff1aa0c8f7ea00a22b6c4
DQ Han <hdqhdq58@outlook.com>
2022-03-09 11:15:20 +0900

    Update pretrained_agents_play_with_each_other.py

--- pymahjong/test/pretrained_agents_play_with_each_other.py
+++ pymahjong/test/pretrained_agents_play_with_each_other.py
@@ -2,6 +2,7 @@ import time
 import env_pymahjong
 import numpy as np
 import torch
+import pymahjong as pm
 
 
 def play_mahjong(agent, num_games=100, verbose=1):
@@ -20,59 +21,63 @@ def play_mahjong(agent, num_games=100, verbose=1):
 
     while game < num_games:
 
-        # try:
-        env.reset()
+        try:
 
-        while not env.is_over():
+            env.reset()
 
-            curr_player_id = env.get_curr_player_id()
+            while not env.is_over():
 
-            # --------- get decision information -------------
+                curr_player_id = env.get_curr_player_id()
 
-            valid_actions_mask = env.get_valid_actions(curr_player_id, nhot=True)
-            executor_obs = env.get_obs(curr_player_id)
+                # --------- get decision information -------------
 
-            oracle_obs = env.get_oracle_obs(curr_player_id)
-            # full_obs = env.get_full_obs(curr_player_id)
-            # full_obs = concat([executor_obs, oracle_obs], axis=0)
+                valid_actions_mask = env.get_valid_actions(curr_player_id, nhot=True)
+                executor_obs = env.get_obs(curr_player_id)
 
-            # --------- make decision -------------
+                oracle_obs = env.get_oracle_obs(curr_player_id)
+                # full_obs = env.get_full_obs(curr_player_id)
+                # full_obs = concat([executor_obs, oracle_obs], axis=0)
 
-            if agent == "random":
-                a = np.random.choice(np.argwhere(valid_actions_mask).reshape([-1]))
-            else:
-                a = agent.select(executor_obs, oracle_obs, action_mask=valid_actions_mask, greedy=True)
+                # --------- make decision -------------
 
-            env.step(a)
+                if agent == "random":
+                    a = np.random.choice(np.argwhere(valid_actions_mask).reshape([-1]))
+                else:
+                    a = agent.select(executor_obs, oracle_obs, action_mask=valid_actions_mask, greedy=True)
 
-        # ----------------------- get result ---------------------------------
+                env.step(a)
 
-        payoffs = env.get_payoffs()
-        if verbose >= 2:
-            print("Game {}, result: {}".format(game, payoffs))
+            # ----------------------- get result ---------------------------------
 
-        for winner in env.t.get_result().winner:
-            stat["agari_points"][winner] += payoffs[winner]
-            stat["agari_games"][winner] += 1
+            payoffs = env.get_payoffs()
+            if verbose >= 2:
+                print("Game {}, result: {}".format(game, payoffs))
 
-        if len(env.t.get_result().loser) == 1:
-            stat["houjyuu_games"][env.t.get_result().loser] += 1
-        else:
-            stat["tsumo_games"][env.t.get_result().winner] += 1
+            if env.t.get_result().result_type == pm.ResultType.RonAgari:
+                winner = np.argwhere(payoffs > 0).flatten()
+                stat["agari_points"][winner] += payoffs[winner]
+                stat["agari_games"][winner] += 1
+                loser = np.argmin(payoffs)
+                stat["houjyuu_games"][loser] += 1
+            if env.t.get_result().result_type == pm.ResultType.TsumoAgari:
+                winner = np.argmax(payoffs)
+                stat["agari_points"][winner] += payoffs[winner]
+                stat["agari_games"][winner] += 1
+                stat["tsumo_games"][env.t.get_result().winner] += 1
 
-        success_games += 1
-        game += 1
+            success_games += 1
+            game += 1
 
-        if verbose >= 1 and game % 100 == 0:
-            print("------------------------ {} games statistics -----------------------".format(success_games))
-            print("win rate:                    ", np.array2string(100 * stat["agari_games"] / success_games, precision=2, separator="  "))
-            print("tsumo rate:                  ", np.array2string(100 * stat["tsumo_games"] / stat["agari_games"], precision=2, separator="  "))
-            print("houjyuu rate:                ", np.array2string(100 * stat["houjyuu_games"] / success_games, precision=2, separator="  "))
-            print("average agari points (x100): ", np.array2string(0.01 * stat["agari_points"] / stat["agari_games"], precision=2, separator="  "))
-            # print("----------------------------------------------------------------------")
-        # except:
-        #     game += 1
-        #     continue
+            if verbose >= 1 and game % 100 == 0:
+                print("------------------------ {} games statistics -----------------------".format(success_games))
+                print("win rate:                    ", np.array2string(100 * stat["agari_games"] / success_games, precision=2, separator="  "))
+                print("tsumo rate:                  ", np.array2string(100 * stat["tsumo_games"] / stat["agari_games"], precision=2, separator="  "))
+                print("houjyuu rate:                ", np.array2string(100 * stat["houjyuu_games"] / success_games, precision=2, separator="  "))
+                print("average agari points (x100): ", np.array2string(0.01 * stat["agari_points"] / stat["agari_games"], precision=2, separator="  "))
+                # print("----------------------------------------------------------------------")
+        except:
+            game += 1
+            continue
 
     print("Total {} game, {} without error, takes {} s".format(num_games, success_games, time.time() - start_time))
 
