193589bc2c3f6924cbc423eab0317e1207d3a606
Agony <survivalonlysoul@163.com>
2022-03-31 10:44:22 +0800

    Update RoundToWin.cpp

--- Mahjong/RoundToWin.cpp
+++ Mahjong/RoundToWin.cpp
@@ -41,8 +41,8 @@ namespace syanten{
             ss >> code_str;
             uint32_t key=0;
             int value[4];
-            for(int i=0; i<9; i++){
-                key += tile_to_bit[i] * (code_str[i]-'0');
+            for (int i = 0; i < 9; i++) {
+                key += tile_to_bit[i] * (code_str[i] - '0');
             }
             ss >> value[0] >> value[1] >> value[2] >> value[3];
             result[key] = std::make_tuple(value[0], value[1], value[2], value[3]);
@@ -62,49 +62,49 @@ namespace syanten{
     void hand_to_code(const std::vector<Tile*>& hand, uint32_t* code){
         // 1m 2m 6m 9m 1s 3s 5s 7s 1p 1p 1p 1p 1z => [110001001, 101010100, 400000000, 100000000]
         memset(code, 0, 4*sizeof(uint32_t));
-        for(auto iter=hand.cbegin();iter!=hand.cend();iter++){
+        for (auto iter = hand.cbegin(); iter != hand.cend(); iter++) {
             BaseTile t = (*iter)->tile;
-            code[t/(_1p-_1m)] += tile_to_bit[t%(_1p-_1m)];
+            code[t / (_1p - _1m)] += tile_to_bit[t % (_1p - _1m)];
         }
     }
-    int _checkNormal(const uint32_t* hand_code, int num_副露){
-        int ptm=0,ptt=0;
-        for(int j=0;j<3;j++){
+    int _checkNormal(const uint32_t* hand_code, int num_副露) {
+        int ptm = 0, ptt = 0;
+        for (int j = 0; j < 3; j++) {
             int pt1m, pt1t, pt2m, pt2t;
-            std::tie(pt1m, pt1t, pt2m, pt2t)=syanten_map[hand_code[j]];
-            if(pt1m*2+pt1t>=pt2m*2+pt2t){
-                ptm+=pt1m;
-                ptt+=pt1t;
+            std::tie(pt1m, pt1t, pt2m, pt2t) = syanten_map[hand_code[j]];
+            if (pt1m * 2 + pt1t >= pt2m * 2 + pt2t) {
+                ptm += pt1m;
+                ptt += pt1t;
             }
-            else{
-                ptm+=pt2m;
-                ptt+=pt2t;
+            else {
+                ptm += pt2m;
+                ptt += pt2t;
             }
         }
 
-        for(int i=0;i<7;i++){
-            int num = 0b111 & (hand_code[3] >> (3*i));
-            if(num>=3){
+        for (int i = 0; i < 7; i++) {
+            int num = 0b111 & (hand_code[3] >> (3 * i));
+            if (num >= 3) {
                 ptm++; // 面子
             }
-            else if(num>=2){
+            else if (num >= 2) {
                 ptt++; // 雀头
             }
         }
         while (ptm + ptt > 4 - num_副露 && ptt > 0) ptt--;
         while (ptm + ptt > 4 - num_副露) ptm--;
-        return 9-ptm*2-ptt - num_副露 * 2;
+        return 9 - ptm * 2 - ptt - num_副露 * 2;
     }
     int normalRoundToWin(const std::vector<Tile*>& hand, int num_副露){
         int result = 14;
         uint32_t hand_code[4];
         hand_to_code(hand, hand_code);
-        for(int i=_1m; i<=_7z; i++){
-            int num = 0b111 & (hand_code[i/(_1p-_1m)] >> (3*(i%(_1p-_1m))));
-            if(num >= 2) {
-                hand_code[i/(_1p-_1m)] -= 2*tile_to_bit[i%(_1p-_1m)];
-                result = std::min(result, _checkNormal(hand_code, num_副露)-1);
-                hand_code[i/(_1p-_1m)] += 2*tile_to_bit[i%(_1p-_1m)];
+        for (int i = _1m; i <= _7z; i++) {
+            int num = 0b111 & (hand_code[i / (_1p - _1m)] >> (3 * (i % (_1p - _1m))));
+            if (num >= 2) {
+                hand_code[i / (_1p - _1m)] -= 2 * tile_to_bit[i % (_1p - _1m)];
+                result = std::min(result, _checkNormal(hand_code, num_副露) - 1);
+                hand_code[i / (_1p - _1m)] += 2 * tile_to_bit[i % (_1p - _1m)];
             }
         }
         result = std::min(result, _checkNormal(hand_code, num_副露));
