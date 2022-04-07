05f953fbc494e207c6098149f4cda48adb774142
Agony <survivalonlysoul@163.com>
2022-04-07 20:33:22 +0800

    Update __init__.py

--- pymahjong/__init__.py
+++ pymahjong/__init__.py
@@ -1,3 +1,7 @@
+import MahjongPyWrapper
+print(dir(MahjongPyWrapper))
+del MahjongPyWrapper
+
 from .env_pymahjong import MahjongEnv, SingleAgentMahjongEnv
 from .test import test, test_with_pretrained
 from .tenhou_paipu_check import paipu_replay, paipu_replay_1
